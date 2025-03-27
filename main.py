db3310fcf36d9cea08b6a087c72e1d2f6400fc45
zakes1NDAH <nzimandezakhele9@gmail.com>
2025-03-27 11:12:22 +0200

    menu

---
+++ main.py
@@ -0,0 +1,14 @@
+tasks=[]
+
+if __name__ == "__main__":
+    ##Create a loop to run the app
+
+    print("Welcome to the to do list app :")
+    while True:
+        print("\n")
+        print("Please select on of the following options")
+        print("--------------------------------------------")
+        print("1, Add a new task")
+        print("2, Delete a task")
+        print("3. List tasks")
+        print("4. Quit")
